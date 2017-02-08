e6c9fec4066c3a42b5a3a45bbf13862bb44c0c96
lawlug026 <ujwalgoyal.2@gmail.com>
2017-02-08 20:07:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,8 @@ console.log('Loaded!');
 var button = document.getElementById("first");
 button.onclick = function () {
     
-    
+    counter=counter+1;
+    var first_read=document.getElementById("first_read");
+    first_read.innerHTML=counter.toString();
     
 };
