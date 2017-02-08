edd101d5236d4f32be349bb71d400a2825802c99
lawlug026 <ujwalgoyal.2@gmail.com>
2017-02-08 21:11:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
-
+/*
 var button = document.getElementById("first");
 var click_btn = function () {
     
@@ -22,4 +22,28 @@ var click_btn = function () {
    request.open('GET','http://lawlug026.imad.hasura-app.io/articleone.html', true);
    request.send(null);
     
+};*/
+
+
+var button=document.getElementById('first');
+var func_click_me=function(){
+    //Create a request to counter endpoint
+    var request=new XMLHttpRequest();
+    //Capture the response & store it in a variable
+    request.onreadystatechange = function(){
+      if(request.readyState===XMLHttpRequest.DONE){
+          //Take sone action
+          if(request.status==200)
+          {
+              var counter=request.responseText;
+               var span=document.getElementById('span');
+                span.innerHTML=counter.toString();
+          } 
+          
+      }//not done yet  
+    };
+    
+    //Make a request
+    request.open('GET', "http://lawlug026.imad.hasura-app.io/articleone.html", true);
+    request.send(null);
 };
