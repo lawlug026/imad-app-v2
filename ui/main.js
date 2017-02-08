6090407e22fe69aa4c2b8e89b31ee71c41591d3c
lawlug026 <ujwalgoyal.2@gmail.com>
2017-02-08 20:50:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onclick = function () {
     
     request.onReadyStateChange = function(){
       if (request.readyState===XMLHttpRequest.DONE){
-          if(request.status===200)
+          if(request.status==200)
           {
               var counter=request.responseText;
                var first_read=document.getElementById("spn");
