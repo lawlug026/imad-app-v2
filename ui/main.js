17201ba380ac81c31118aa0ff25474d29f10ede0
lawlug026 <ujwalgoyal.2@gmail.com>
2017-02-08 20:39:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,22 @@ console.log('Loaded!');
 var button = document.getElementById("first");
 button.onclick = function () {
     
-    counter=counter+1;
-    var first_read=document.getElementById("first_read");
+    var request = new XMLHttpRequest();
+    
+    request.onReadyStateChanged = function(){
+      if (request.readyState===XMLHttpRequest.DONE){
+          if(request.status===200)
+          {
+              var counter=request.responseText;
+               var first_read=document.getElementById("first_read");
     first_read.innerHTML=counter.toString();
+          }
+          
+          
+      }  
+    };
+    
+   request.open('GET','http://http://lawlug026.imad.hasura-app.io/articleone.html', true);
+   request.send(null);
     
 };
