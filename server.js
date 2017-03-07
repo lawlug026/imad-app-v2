ef926b8ce6b2330b55560ce272f75b39ce512246
lawlug026 <ujwalgoyal.2@gmail.com>
2017-03-07 20:46:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.get('/articles/:articleName', function(req,res){
         }
         else
         {
-            var articleData= res.send(result.rows[0]);
+            var articleData= result.rows[0];
             res.send(createTemplate(articleData)); 
         }
         
