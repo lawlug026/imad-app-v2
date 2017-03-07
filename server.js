505dbaa7d33b3cd525c22126394b9f9417fe1ae1
lawlug026 <ujwalgoyal.2@gmail.com>
2017-03-07 20:32:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.get('/test-db', function (req, res) {
 var counter=0;
 app.get('/articles/:articleName', function(req,res){
   
-  pool.query("SELECT * FROM ARTICLE WHERE title='"+req.params.articleName+"'", function(err, result){
+  pool.query("SELECT * FROM article WHERE title='"+req.params.articleName+"'", function(err, result){
     if(err)
     {
         res.status(500).send(err.toString());
